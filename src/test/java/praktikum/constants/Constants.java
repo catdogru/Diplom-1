a9943d0ed59a5dd59bda0a39ec0a3f5f6431c0e1
daria.i <dvigina@yandex.ru>
2024-06-14 01:49:34 +0300

    review

--- src/test/java/praktikum/constants/Constants.java
+++ src/test/java/praktikum/constants/Constants.java
@@ -1,11 +1,11 @@
 package praktikum.constants;
 
-public interface Constants {
-    String DEFAULT_BUN_NAME = "Йа_булочко";
-    float DEFAULT_BUN_PRICE = 50F;
+public class Constants {
+    public static final String DEFAULT_BUN_NAME = "Йа_булочко";
+    public static final float DEFAULT_BUN_PRICE = 50F;
 
-    String DEFAULT_INGREDIENT_NAME = "Йа_ингредиент";
-    float DEFAULT_INGREDIENT_PRICE = 25F;
+    public static final String DEFAULT_INGREDIENT_NAME = "Йа_ингредиент";
+    public static final float DEFAULT_INGREDIENT_PRICE = 25F;
 
-    int ONE_INVOCATION = 1;
+    public static final int ONE_INVOCATION = 1;
 }
